28b1153886c3065dd610c841b68d57c1a882563a
Mukul Rajak <105576620+IAMMUKULR@users.noreply.github.com>
2022-10-09 15:19:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function random_image() {
   var select = img_array[index];
   var ch1 = document.getElementById("check1");
   if (ch1.checked == true) {
-    document.getElementById("img_show1").src = `./${select}`;
+    document.getElementById("img_show1").src = `${select}`;
 
     setTimeout(doSomething, 200);
     function doSomething() {
@@ -25,7 +25,7 @@ function random_image() {
   }
   var ch2 = document.getElementById("check2");
   if (ch2.checked == true) {
-    document.getElementById("img_show2").src = `./${select}`;
+    document.getElementById("img_show2").src = `${select}`;
     setTimeout(doSomething, 200);
     function doSomething() {
       document.getElementById("check2").checked = false;
@@ -37,7 +37,7 @@ function random_image() {
   }
   var ch3 = document.getElementById("check3");
   if (ch3.checked == true) {
-    document.getElementById("img_show3").src = `./${select}`;
+    document.getElementById("img_show3").src = `${select}`;
     setTimeout(doSomething, 200);
     function doSomething() {
       document.getElementById("check3").checked = false;
@@ -45,7 +45,7 @@ function random_image() {
   }
   var ch4 = document.getElementById("check4");
   if (ch4.checked == true) {
-    document.getElementById("img_show4").src = `./${select}`;
+    document.getElementById("img_show4").src = `${select}`;
     setTimeout(doSomething, 200);
     function doSomething() {
       document.getElementById("check4").checked = false;
